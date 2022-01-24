8643adcf7abe4c425c4b4c7b445f70200e6adee1
Kotaprolu Hemanth <kotaproluhemanth@pop-os.localdomain>
2022-01-24 22:05:13 +0530

    Learnt Hooks

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('Mario');
+    const [Age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('Hemanth');
+        setAge(21);
+    };
     return (
         <div className='home'>
             <h1>Homepage</h1>
+            <p>
+                {name} is {Age} years old
+            </p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 };
