c5e27b6c200367a50a2b5cc87b1c704af8fc06f6
Kotaprolu Hemanth <kotaproluhemanth@pop-os.localdomain>
2022-01-25 11:17:00 +0530

    created JSON-server

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [name, setName] = useState('Mario');
-    const [Age, setAge] = useState(25);
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'My new website',
+            body: 'lorem ipsum...',
+            author: 'mario',
+            id: 1,
+        },
+        {
+            title: 'Welcome party!',
+            body: 'lorem ipsum...',
+            author: 'yoshi',
+            id: 2,
+        },
+        {
+            title: 'Web dev top tips',
+            body: 'lorem ipsum...',
+            author: 'mario',
+            id: 3,
+        },
+    ]);
 
-    const handleClick = () => {
-        setName('Hemanth');
-        setAge(21);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
     };
+
+    useEffect(() => {
+        console.log('useEffect ran');
+    }, []);
+
     return (
         <div className='home'>
-            <h1>Homepage</h1>
-            <p>
-                {name} is {Age} years old
-            </p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList
+                blogs={blogs}
+                title='All Blogs!'
+                handleDelete={handleDelete}
+            />
         </div>
     );
 };
